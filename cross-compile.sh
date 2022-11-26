5d9cbb43afc81f688be0321b0b90a4a087b602ba
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2022-11-26 15:53:55 +0800

    Add support for loong64

--- cross-compile.sh
+++ cross-compile.sh
@@ -19,7 +19,7 @@ function buildem() {
   done
 }
 
-GOARCHES="386 amd64 arm arm64 ppc64 ppc64le s390x mips mipsle mips64 mips64le"
+GOARCHES="386 amd64 arm arm64 ppc64 ppc64le s390x mips mipsle mips64 mips64le loong64"
 buildem "linux" "$GOARCHES"
 
 GOARCHES_BSD="386 amd64 arm arm64"
